549454dd1063cd1ce4a1fc4a84b08407ef6c9bf0
Zmoore <zmoore371@gmail.com>
2021-09-01 12:14:42 -0400

    able to loop through html to grab all times from h4 elements. have also converted the array into indidividual times using momentjs

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,35 @@
 timeEl = $("#currentDay")
+timeBlockTime = $('.hour') //returns an array, can grab current time with timeBlockTime[i].innerHTML
+// timeArray = 
 
+
+function setBackground() {
+    for(i=0; i<timeBlockTime.length; i++) {
+        console.log(timeBlockTime[i].innerHTML)
+        rowTime = timeBlockTime[i].innerHTML
+        rowTime = moment(rowTime, "h, A" ).format("h A")
+        console.log(rowTime)
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+console.log(timeBlockTime)
+// console.log(timeArray)
 
 
 function updateTime() {
     timeEl.html(moment().format("ddd, MMMM Do hh:mm:ss"))
 }
 setInterval(updateTime, 1000)
+
+
+setBackground()
